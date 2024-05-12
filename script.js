0ddba58d710a2cdbeb4ef53a7cebcc985e8ee3a8
BhavnaChaudhary <33368486+BhavnaChaudhary@users.noreply.github.com>
2024-05-12 16:29:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function fibonacci(num) {
-// your code here
+  // Base Case
+  if (num === 0 || num === 1) return num;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
